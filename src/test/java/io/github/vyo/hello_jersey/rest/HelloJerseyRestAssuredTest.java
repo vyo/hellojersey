28a553c66dc109d242306b928d3cb8f003d53ec1
Manuel Weidmann <weidmann.manuel@gmail.com>
2015-03-05 11:03:24 +0100

    Run RESTassured only if local server is live

--- src/test/java/io/github/vyo/hello_jersey/rest/HelloJerseyRestAssuredTest.java
+++ src/test/java/io/github/vyo/hello_jersey/rest/HelloJerseyRestAssuredTest.java
@@ -2,22 +2,44 @@ package io.github.vyo.hello_jersey.rest;
 
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.path.json.JsonPath;
+import com.jayway.restassured.response.Response;
 import org.junit.After;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.expect;
 import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.config.ConnectionConfig.connectionConfig;
+import static com.jayway.restassured.config.HttpClientConfig.httpClientConfig;
+import static com.jayway.restassured.config.RestAssuredConfig.newConfig;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeTrue;
 
 /**
  * Created by Manuel Weidmann on 03.03.2015.
  */
 public class HelloJerseyRestAssuredTest {
-    @Before
-    public void setUp() {
+
+    static boolean connectionExists = false;
+
+    @BeforeClass
+    public static void setUp() {
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = 8080;
         RestAssured.basePath = "/app";
+        RestAssured.config = newConfig().connectionConfig(connectionConfig()
+                .closeIdleConnectionsAfterEachResponse()).httpClient
+                (httpClientConfig().setParam("CONNECTION_MANAGER_TIMEOUT", 50));
+
+        try {
+            Response response = expect().statusCode(200).contentType(ContentType.JSON).when()
+                    .get("/rest/hellojersey/hello");
+
+            connectionExists = "Hello Jersey!".equals(new JsonPath(response.getBody().asString()).getString
+                    ("message"));
+        } catch (Exception e) {
+        }
     }
 
     @After
@@ -26,12 +48,16 @@ public class HelloJerseyRestAssuredTest {
 
     @Test
     public void testHello() {
-        expect().statusCode(200).contentType(ContentType.JSON).when()
+        assumeTrue(connectionExists);
+        Response response = expect().statusCode(200).contentType(ContentType.JSON).when()
                 .get("/rest/hellojersey/hello");
+
+        assertEquals("Hello Jersey!", new JsonPath(response.getBody().asString()).getString("message"));
     }
 
     @Test
     public void testHelloEcho() {
+        assumeTrue(connectionExists);
         given().pathParam("echo", "echo.").expect().statusCode(200).when().get("/rest/hellojersey/echo/{echo}");
     }
 }
